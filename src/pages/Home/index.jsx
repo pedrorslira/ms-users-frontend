7dc12f08c2ebeba959629587ea6727e353fca8f9
Pedro Ribeiro <pedroribeiro64@outlook.com>
2025-04-24 16:02:41 -0300

    chore: improved home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import "./styles.css";
 
 export default function index() {
   return (
-    <div>
-      <h1>CRUD TEST</h1>
+    <div className="home-container">
+      <h1>Welcome to the User Management App</h1>
+      <h2>Please use the top navigation bar to access user features.</h2>
     </div>
   );
 }
